a52bb42be41dd44f5697a24611bdfee110865891
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-09-21 09:17:31 +0000

    Create ads.js

---
+++ public/js/ads.js
@@ -0,0 +1,8 @@
+// Gestion basique des publicités
+document.addEventListener("DOMContentLoaded", () => {
+  try {
+    (adsbygoogle = window.adsbygoogle || []).push({});
+  } catch (err) {
+    console.warn("Google Ads pas encore actif :", err);
+  }
+});
